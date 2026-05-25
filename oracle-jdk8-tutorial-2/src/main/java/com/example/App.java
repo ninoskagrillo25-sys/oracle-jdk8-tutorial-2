c9b49f01f8c208910c713efafb5f0216b7918788
ninoskagrillo25-sys <ninoskagrillo25@gmail.com>
2026-05-25 18:13:19 +0200

    PATRON BUILDER DEL LOMBOK EXPLICADO

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -13,25 +13,51 @@ public class App {
 		/*
 		 * ¿Como imprimir en la consola el objeto referenciado por la variable manzana1
 		 */
-
 		System.out.println("------- Imprimiendo manzana1 ------- ");
 		System.out.println(manzana1.toString());
 
-		// Si quisiera crear un objeto manzana y solamente especificar el color de la
-		// Manzana,
-		// me daria error porque yo no tengo un constructor que reciba un solo parametro
-
-		Manzana manzana2 = new Manzana("Golden");
+		/* Utilizando Lombok no necesitamos tener todos los constructores defiunidos,
+		 * solamente el constructor sin parametros o por dfecxto y el constructor
+		 * que recibe todos los campos de la clase
+		 * Utilizando EL PATRON BUILDER DE LOMBOK, es posible construir cualquier
+		 * objeton aunque no tengas su constructor explicitamente, y tampoco estariamos
+		 * obligados a suministrar los campos o propiedades del objeto en un orden
+		 * determinado*/
+		
+		//El Patron Builder NO fue inventado por lombok, pero lo ha mejorado bastante;
+		
+		//El Patron Builder es patron factoria, es decir, creacional,
+		// que permite creainstanciar objetos sin tener explicitamente su
+		// su constructor
+		
+		//Manzana manzana 2 = new Manzana("Golden");
+		
+		Manzana manzana2 = Manzana.builder()
+				.variedad("Golden")
+				.build();
+		
+		
 		System.out.println("------- Imprimiendo manzana2 ------- ");
 		System.out.println(manzana2);
 
 		/*
 		 * ¿ Como comparar dos objetos Manzana para saber si son iguales o no?
 		 * 
-		 * Vamos a crear dos objetos Manzanas que sean de la misma vcariedad y precio
-		 */
-		Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
-		Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
+		 * Vamos a crear dos objetos Manzanas que sean de la misma vcariedad y precio*/
+		
+		//Manzana manzana3 = new Manzana("reineta, new BigDecimal(2.0));
+		
+		Manzana manzana3 = Manzana.builder()
+				.variedad("reineta")
+				.precio(new BigDecimal(2.0))
+				.build();
+		
+		////Manzana manzana4 = new Manzana("reineta, new Bigdecimal(2.0));
+		Manzana manzana4 = Manzana.builder()
+				.variedad("reineta")
+				.precio(new BigDecimal(2.0))
+				.build();
+		
 
 		int x = 5;
 		int y = 5;
@@ -46,10 +72,14 @@ public class App {
 		// si no se cumple la condicion
 		// se podra ejecutar la o las sentencias que estan en siguiente bloque.
 
-		if (x == y) {
-			System.out.println("Las variables x e y son iguales");
+ 
+		if (manzana3==manzana4) 
+			
+		{
+			System.out.println("Los objetos manzana 3 y manzana 4 apuntan a la misma direccion");
 		} else {
-			System.out.println("Las variables x e y no son iguales");
+			System.out.println("Los objetos manzana 3 y manzana 4 apuntan a direcciones distintas");
+			
 		}
 		/*
 		 * Para saber si el contenido de dos objetos es el mismo se utiliza el metodo
--- oracle-jdk8-tutorial-2/src/main/java/com/example/Manzana.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/Manzana.java
@@ -3,6 +3,14 @@ package com.example;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Manzana {
 	
 	/* Una clase bien diseñada es aquella que tiene sus propiedades son 
@@ -17,115 +25,11 @@ public class Manzana {
 	private String variedad;
 	private double size; 
 	private double peso;
-	private BigDecimal precio;	
+	private BigDecimal precio;
+}
+
 		
 	// Los contructores (Metodos que tienen el mismo nombre que la clase)
 	// y que son los que permiten crear o instanciar los objetos de este tipo de claseConstructor de la clase (es un metodo especial que se llama igual que la clase y no tiene tipo de retorno)
 
-	public Manzana(String variedad) {
-		super();
-		this.variedad = variedad;
-	}
-
-	public Manzana() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Manzana(String color, String sabor, String variedad, double size, double peso, BigDecimal precio) {
-		super();
-		this.color = color;
-		this.sabor = sabor;
-		this.variedad = variedad;
-		this.size = size;
-		this.peso = peso;
-		this.precio = precio;
-	}
-
-	// Metodos getters and setters (son los que permiten acceder a las propiedades privadas de la clase)(metodos publicos para manejar las propiedades privadas)
-
-
-	public String getColor() {
-		return color;
-	}
-	public Manzana(String variedad, BigDecimal precio) {
-		super();
-		this.variedad = variedad;
-		this.precio = precio;
-	}
-
-	public String getPeso() {
-		return String.valueOf(peso);
-	}
-	
-	public String getSabor() {
-		return sabor;
-	}
-
-	public void setSabor(String sabor) {
-		this.sabor = sabor;
-	}
-
-	public String getVariedad() {
-		return variedad;
-	}
-
-	public void setVariedad(String variedad) {
-		this.variedad = variedad;
-	}
-
-	public double getSize() {
-		return size;
-	}
-
-	public void setSize(double size) {
-		this.size = size;
-	}
-
-	public BigDecimal getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(BigDecimal precio) {
-		this.precio = precio;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public void setPeso(double peso) {
-		this.peso = peso;
-	}
-
-	@Override
-	public String toString() {
-		return "Manzana [color=" + color + ", sabor=" + sabor + ", variedad=" + variedad + ", size=" + size + ", peso="
-				+ peso + ", precio=" + precio + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(precio, variedad);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Manzana other = (Manzana) obj;
-		return Objects.equals(precio, other.precio) && Objects.equals(variedad, other.variedad);
-	}
-
-	
-	// Otros metodos (No estan asociados a ninguna propiedad concreta, sino que son acciones o comportamientos sino a varias)
-	
-	
-	
-	
-}
 	
